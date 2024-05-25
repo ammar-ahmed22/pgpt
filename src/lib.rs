463a181b58523cf610569ad5163bd8e746bccbed
Ammar Ahmed <ammar.ahmed1@uwaterloo.ca>
2024-05-25 03:20:52 -0400

    feat: subcommands
    Major updates to the CLI
    
    Now uses subcommands, use pgpt query to query chatgpt
    use pgpt config to set, show, clear configuration values
    can set cached responses and set context for queries
    
    Still have not implemented the caching and context

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,23 @@
 pub mod config;
 pub mod encryption;
+pub mod gpt;
 
 use anyhow::Context;
-use reqwest::blocking::{ Client, Response };
-use reqwest::header::{ HeaderMap, HeaderValue, CONTENT_TYPE, USER_AGENT, AUTHORIZATION };
-use serde::Deserialize;
+use gpt::{GPTClient, GPTQuery, GPTResponse, GPTRole};
+// use reqwest::blocking::{ Client, Response };
+// use reqwest::header::{ HeaderMap, HeaderValue, CONTENT_TYPE, USER_AGENT, AUTHORIZATION };
+// use serde::Deserialize;
 use colored::*;
 use indicatif::{ ProgressBar, ProgressStyle };
+// use std::str::FromStr;
 use std::sync::Arc;
+use std::thread::JoinHandle;
 use rand::Rng;
 use termimad::{ MadSkin, rgb };
 use termimad::crossterm::style::Color::*;
+use config::model::Model;
 
-const COMPLETION_URL: &'static str = "https://api.openai.com/v1/chat/completions";
+// const COMPLETION_URL: &'static str = "https://api.openai.com/v1/chat/completions";
 const LOADING_MESSAGES: [&'static str; 10] = [
   "Consulting neural network...",
   "Bribing data set...",
@@ -27,83 +32,85 @@ const LOADING_MESSAGES: [&'static str; 10] = [
 ];
 
 
-#[derive(Deserialize, Debug)]
-#[allow(unused)]
-pub struct CompletionResponse {
-  choices: Vec<Choice>,
-  created: i64,
-  id: String,
-  model: String,
-  object: String,
-  usage: Usage
-}
+// #[derive(Deserialize, Debug)]
+// #[allow(unused)]
+// pub struct CompletionResponse {
+//   choices: Vec<Choice>,
+//   created: i64,
+//   id: String,
+//   model: String,
+//   object: String,
+//   usage: Usage
+// }
 
-#[derive(Deserialize, Debug)]
-#[allow(unused)]
-pub struct Choice {
-  finish_reason: String,
-  index: i32,
-  message: Message,
-}
+// #[derive(Deserialize, Debug)]
+// #[allow(unused)]
+// pub struct Choice {
+//   finish_reason: String,
+//   index: i32,
+//   message: Message,
+// }
 
-#[derive(Deserialize, Debug)]
-#[allow(unused)]
-pub struct Message {
-  content: String,
-  role: String,
-}
+// #[derive(Deserialize, Debug)]
+// #[allow(unused)]
+// pub struct Message {
+//   content: String,
+//   role: String,
+// }
 
-#[derive(Deserialize, Debug)]
-#[allow(unused)]
-pub struct Usage {
-  completion_tokens: i32,
-  prompt_tokens: i32,
-  total_tokens: i32,
-}
+// #[derive(Deserialize, Debug)]
+// #[allow(unused)]
+// pub struct Usage {
+//   completion_tokens: i32,
+//   prompt_tokens: i32,
+//   total_tokens: i32,
+// }
+
+// impl Usage {
+//   /// Calculates the total cost of the query
+//   pub fn total_cost(&self, model: &Model) -> f64 {
+//     ((self.prompt_tokens as f64) * model.prompt_cost()) + ((self.completion_tokens as f64) * model.completion_cost())
+//   }
+// }
 
 /// Creates the HTTP request client
-fn create_client(api_key: &str) -> anyhow::Result<Client> {
-  let auth_val = format!("Bearer {}", api_key);
-  let mut headers = HeaderMap::new();
-  headers.insert(CONTENT_TYPE, HeaderValue::from_static("application/json"));
-  headers.insert(USER_AGENT, HeaderValue::from_static("pgpt/0.1.0"));
-  headers.insert(AUTHORIZATION, HeaderValue::from_str(&auth_val)?);
-  let client = Client::builder()
-    .default_headers(headers)
-    .build()?;
-  Ok(client)
-}
+// fn create_client(api_key: &str) -> anyhow::Result<Client> {
+//   let auth_val = format!("Bearer {}", api_key);
+//   let mut headers = HeaderMap::new();
+//   headers.insert(CONTENT_TYPE, HeaderValue::from_static("application/json"));
+//   headers.insert(USER_AGENT, HeaderValue::from_static("pgpt/0.1.0"));
+//   headers.insert(AUTHORIZATION, HeaderValue::from_str(&auth_val)?);
+//   let client = Client::builder()
+//     .default_headers(headers)
+//     .build()?;
+//   Ok(client)
+// }
 
 /// Makes the ChatGPT request and returns the response
-fn query_gpt(args: Arc<config::CLIArgs>, config: Arc<config::Config>) -> anyhow::Result<CompletionResponse> {
-  let client = create_client(&config.api_key)?;
-  let body = serde_json::json!({
-    "model": args.model.api_model(),
-    "messages": [
-      {
-        "role": "user",
-        "content": &args.query
-      }
-    ]
-  });
-  let response: Response = client.post(COMPLETION_URL)
-    .json(&body)
-    .send()?;
-  if response.status().is_success() {
-    let completion: CompletionResponse = response.json()?;
-    return Ok(completion);
-  } else {
-    let mut err: serde_json::Value = response.json()?;
-    let message = err["error"]["message"].take();
-    return Err(anyhow::anyhow!(message));
-  }
-}
+// fn query_gpt(args: Arc<config::CLIArgs>, config: Arc<config::Config>) -> anyhow::Result<CompletionResponse> {
+//   let client = create_client(&config.api_key)?;
+//   let body = serde_json::json!({
+//     "model": args.model.api_model(),
+//     "messages": [
+//       {
+//         "role": "user",
+//         "content": &args.query
+//       }
+//     ]
+//   });
+//   let response: Response = client.post(COMPLETION_URL)
+//     .json(&body)
+//     .send()?;
+//   if response.status().is_success() {
+//     let completion: CompletionResponse = response.json()?;
+//     return Ok(completion);
+//   } else {
+//     let mut err: serde_json::Value = response.json()?;
+//     let message = err["error"]["message"].take();
+//     return Err(anyhow::anyhow!(message));
+//   }
+// }
 
-/// Calculates the total cost for the query and response
-fn calculate_cost(model: &config::Model, usage: &Usage) -> f64 {
-  let cost = ((usage.prompt_tokens as f64) * model.prompt_cost()) + ((usage.completion_tokens as f64) * model.completion_cost());
-  return cost;
-}
 
 /// Creates the loading spinner
 fn create_spinner() -> anyhow::Result<ProgressBar> {
@@ -130,14 +137,72 @@ fn create_skin() -> MadSkin {
 }
  
 /// Runs the CLI
-pub fn run(args: Arc<config::CLIArgs>, config: Arc<config::Config>) -> anyhow::Result<()> {
-  let spinner = create_spinner()?;
-  let skin = create_skin();
+// pub fn run(args: Arc<config::CLIArgs>, config: Arc<config::Config>) -> anyhow::Result<()> {
+//   let spinner = create_spinner()?;
+//   let skin = create_skin();
+//   let args_clone = Arc::clone(&args);
+//   let config_clone = Arc::clone(&config);
+//   let handle = std::thread::spawn(move || {
+//     let response = query_gpt(args_clone, config_clone).unwrap();
+//     response
+//   });
+
+//   while !handle.is_finished() {
+//     spinner.tick();
+//     std::thread::sleep(std::time::Duration::from_millis(200));
+//   }
+
+//   spinner.finish_and_clear();
+
+//   match handle.join() {
+//     Ok(response) => {
+//       // println!("response...");
+//       println!("{}", format!("Response from {}", args.model.api_model().magenta()).cyan());
+//       println!("");
+//       println!("{}", skin.term_text(response.choices[0].message.content.as_str()));
+//       println!("");
+//       if args.cost {
+//         println!("{}: ${:.6}", "Cost".green(), calculate_cost(&args.model, &response.usage));
+//       }
+//       Ok(())
+//     },
+//     Err(_) => {
+//       return Err(anyhow::anyhow!("Thread failed to execute!"))
+//     }
+//   }
+// }
+
+pub fn get_model(args: Arc<config::QueryArgs>, config: Arc<config::Config>) -> Model {
+  match &args.model {
+    Some(model) => model.clone(),
+    None => config.model.clone()
+  }
+}
+
+/// Handles logic for query command
+pub fn run_query(args: Arc<config::QueryArgs>, config: Arc<config::Config>) -> anyhow::Result<()> {
+  // Cloning to be used in separate thread
   let args_clone = Arc::clone(&args);
   let config_clone = Arc::clone(&config);
-  let handle = std::thread::spawn(move || {
-    let response = query_gpt(args_clone, config_clone).unwrap();
-    response
+
+  // Visuals
+  let spinner = create_spinner()?;
+  let skin = create_skin();
+
+  let model = match &args.model {
+    Some(model) => model.clone(),
+    None => config.model.clone()
+  };
+  let model_clone = Arc::new(model.clone());
+
+  let handle: JoinHandle<anyhow::Result<GPTResponse>> = std::thread::spawn(move || {
+    let gpt = GPTClient::new(&config_clone.api_key)?;
+    let query = GPTQuery::builder()
+      .model(&model_clone)
+      .message(GPTRole::User, &args_clone.query)
+      .build()?;
+    let response = gpt.query(&query)?;
+    Ok(response)
   });
 
   while !handle.is_finished() {
@@ -145,22 +210,27 @@ pub fn run(args: Arc<config::CLIArgs>, config: Arc<config::Config>) -> anyhow::R
     std::thread::sleep(std::time::Duration::from_millis(200));
   }
 
-  spinner.finish_and_clear();
-
   match handle.join() {
-    Ok(response) => {
-      // println!("response...");
-      println!("{}", format!("Response from {}", args.model.api_model().magenta()).cyan());
-      println!("");
-      println!("{}", skin.term_text(response.choices[0].message.content.as_str()));
-      println!("");
-      if args.cost {
-        println!("{}: ${:.6}", "Cost".green(), calculate_cost(&args.model, &response.usage));
+    Ok(result) => {
+      match result {
+        Ok(response) => {
+          println!("{}", format!("Response from {}", response.model.magenta()).cyan());
+          println!("");
+          println!("{}", skin.term_text(response.choices[0].message.content.as_str()));
+          println!("");
+          if args.cost {
+            println!("{}: ${:.6}", "Cost".green(), response.usage.total_cost(&model));
+          }
+        },
+        Err(e) => {
+          return Err(anyhow::anyhow!(e))
+        }
       }
-      Ok(())
     },
     Err(_) => {
       return Err(anyhow::anyhow!("Thread failed to execute!"))
     }
   }
+
+  Ok(()) 
 }
